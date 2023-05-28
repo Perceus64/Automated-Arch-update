ab5fdea7019ce8517f19076a800ccb5123b17a72
Charbel Bachaalany <cbachaalany@gmail.com>
2023-05-28 18:52:01 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# Update the package lists and upgrade the system
-sudo pacman -Syu --noconfirm
+while true; do
+    # Update the package lists and upgrade the system
+    sudo pacman -Syu --noconfirm
 
-# Clean up unused packages and old versions of installed packages
-sudo pacman -Sc --noconfirm
+    # Clean up unused packages and old versions of installed packages
+    sudo pacman -Sc --noconfirm
 
-# Remove orphaned packages
-sudo pacman -Rns $(pacman -Qdtq) --noconfirm
+    # Remove orphaned packages
+    sudo pacman -Rns $(pacman -Qdtq) --noconfirm
 
-# Update the AUR packages using yay (assuming you have yay installed)
-yay -Syu --noconfirm
+    # Update the AUR packages using yay (assuming you have yay installed)
+    yay -Syu --noconfirm
 
-# Clean up unused AUR packages
-yay -Sc --noconfirm
+    # Clean up unused AUR packages
+    yay -Sc --noconfirm
+
+    # Sleep for a specific duration before the next update (e.g., 24 hours)
+    sleep 24h
+done
